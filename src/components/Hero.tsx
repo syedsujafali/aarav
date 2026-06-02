3a0ad518f3a9dc8cedde76ac8b3fcea0a172669d
Cmax <https://github.com/syedsujafali/renaissance-elevated.git>
2026-06-02 18:12:34 +0530

    Remove scroll indicator from Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -80,11 +80,7 @@ export default function Hero() {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 hidden md:flex flex-col items-center gap-2 text-[#f5efe4]/40">
-        <span className="text-[10px] tracking-[0.4em] uppercase">Scroll</span>
-        <div className="w-px h-12 bg-gradient-to-b from-[#c9a35a] to-transparent" />
-      </div>
+      
     </section>
   );
 }
